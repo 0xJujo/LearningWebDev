32461672dea9b7142a0a9d52a567ff158fc0fa47
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-07-04 20:29:27 +0530

    Try simon game again later

--- Section 20/index.js
+++ Section 20/index.js
@@ -3,30 +3,56 @@ var gamePattern=[];
 var randomNumber,randomChosenColour,level=0;
 var gamePattern=[];
 var userChosenPattern=[];
-var started;
+var started=false;
 
-$("body").keydown(function(){
-    if (started===false){
+$(document).keydown(function(){
+    if (!started){
+        $(".level-title").text("Level "+level);
         nextSequence();
+        started=true;
     }
-    started=false;
 });
 
 function nextSequence(){
+    userChosenPattern=[];
     level++;
+    $(".level-title").text("Level "+level);
     randomNumber= Math.floor(Math.random()*4);
     randomChosenColour=buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     playSound(randomChosenColour);
     randomChosenColour="#"+randomChosenColour;
     $(randomChosenColour).fadeOut(50).fadeIn(50);
-    $(".btn").click(function(){
-        var userChosenColour=this.id;
-        userChosenPattern.push(userChosenColour);
-        animatePress(userChosenColour);
-        playSound(userChosenColour);
-    });
+   
 }
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userChosenPattern[currentLevel]){
+        if(userChosenPattern.length===gamePattern.length){
+            setTimeout(function () => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        setTimeout(function () {
+        $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+
+$(".btn").click(function(){
+    var userChosenColour=this.id;
+    userChosenPattern.push(userChosenColour);
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
+    checkAnswer(userChosenPattern.length-1);
+});
 
 function playSound(name){
     var soundLocation="sounds/"+name+".mp3";
@@ -39,4 +65,10 @@ function animatePress(name){
     setTimeout(() => {
         $("#"+name).removeClass("pressed");
     }, 150);
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
 }
