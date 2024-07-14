49a3b1e58eb1a8618b0f96085adb7ffb41446a40
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-07-14 16:14:18 +0530

    Made a QR code generator!

--- Section 23/23.4 QR Code Project/index.js
+++ Section 23/23.4 QR Code Project/index.js
@@ -3,3 +3,22 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import input from '@inquirer/input';
+import qr from 'qr-image';
+import fs from 'fs';
+var answer = await input({ message: 'Enter the website URL you want to make QR of: ' });
+
+fs.writeFile('URL.txt',answer,function(err){
+    if (err) throw err;
+    console.log("File created or edited!");
+});
+
+fs.readFile('URL.txt', 'utf8', function(err, data){
+    if (err) throw err;
+    qrgen(data);
+})
+
+function qrgen(uril){
+    var qr_svg = qr.image(uril);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+}
