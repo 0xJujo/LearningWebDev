e8622189f1193d4dd61807313ce360625019d614
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-08-05 19:04:32 +0530

    React JS

--- Section 36/279-intro-to-jsx-start/279-intro-to-jsx/src/index.jsx
+++
@@ -1,5 +0,0 @@
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
---
+++ Section 36/36.1 Intro to JSX/src/index.js
@@ -0,0 +1,4 @@
+var react = require("react");
+var ReactDom = require("react-dom");
+
+ReactDom.render(<h1>hehe</h1>, document.querySelector("#root"));
