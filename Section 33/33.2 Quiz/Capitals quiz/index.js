369d67deb101312bfc1d192523fdaca3ffdca5af
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-08-21 19:38:03 +0530

    pgSQL

---
+++ Section 33/33.2 Quiz/Capitals quiz/index.js
@@ -0,0 +1,68 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user:"postgres",
+  password:"1234",
+  host:"localhost",
+  port:5432,
+  database: "world",
+});
+let quiz = [];
+db.connect();
+db.query("SELECT * FROM capitals", (err,res)=>{
+  if(err){
+    console.log("Error querying db", err.stack);
+  } else {
+    quiz=res.rows;
+  }
+  db.end();
+});
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capitals.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ Section 33/33.3 World Food/world-food.sql
@@ -0,0 +1,2 @@
+SELECT * FROM public.world_food WHERE country LIKE'%'||'a'
+ORDER BY rice_production DESC 
