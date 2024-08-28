c78f942cdafc8d276675534e947d39cb9367122c
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-08-28 19:49:18 +0530

    Update queries.sql

--- Section 33/33.6 Permalist Project/queries.sql
+++ Section 33/33.6 Permalist Project/queries.sql
@@ -3,10 +3,8 @@ CREATE TABLE items (
   title VARCHAR(100) NOT NULL
 );
 
-INSERT INTO items (title) VALUES ('Buy milk'), ('Finish homework');
+INSERT INTO items (title) VALUES ('Buy milk hehe'), ('Finish homework');
 
 
 
 --zlib http://loginzlib2vrak5zzpcocc3ouizykn6k5qecgj2tzlnab5wcbqhembyd.onion
-
-
