340e7973e218b31395567b7d7d058a8729ec746f
PANKAJ2401-JAGTAP <102534138+PANKAJ2401-JAGTAP@users.noreply.github.com>
2024-08-29 18:43:51 +0530

    test commit

--- Section 33/33.6 Permalist Project/queries.sql
+++ Section 33/33.6 Permalist Project/queries.sql
@@ -5,6 +5,6 @@ CREATE TABLE items (
 
 INSERT INTO items (title) VALUES ('Buy milk hehe'), ('Finish homework');
 
-
+--hi my name is pankaj
 
 --zlib http://loginzlib2vrak5zzpcocc3ouizykn6k5qecgj2tzlnab5wcbqhembyd.onion
